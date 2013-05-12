2ca6622cb087e74ca916ec94f3f11d8a72af3a2f
Stuart P. Bentley <stuart@testtrack4.com>
2013-05-11 22:13:45 -0400

    Refactor to be a bit cleaner + Node-friendly

--- hashblot.js
+++ hashblot.js
@@ -1,14 +1,44 @@
-//Fun, kinda angle-y. Tends to make a lot of birds.
-window.hashblot = function fiveQuadraticsPath (hash){
+(function(){
+  
+"use strict";
+  
+var hashblot = {};
+var sha1;
+
+if(typeof module != "undefined") {
+  module.exports = hashblot;
+  var crypto = require('crypto');
+  sha1 = function sha1(content) {
+    var hash = crypto.createHash('sha1');
+    hash.update(content,'utf8');
+    return hash.digest('hex');
+  };
+} else {
+  window.hashblot = hashblot;
+  sha1 = function sha1(content) {
+    /*global jsSHA*/
+    return new jsSHA(content,'TEXT').getHash('SHA-1','HEX');
+  };
+}
+
+function qp (hash) {
+  var qwords = hash.length / 8;
   var pathNodes = [];
-  pathNodes[0] = 'M '+parseInt(hash.slice(-4,-2),16)
-    +','+parseInt(hash.slice(-2),16);
-  for(var i = 1; i < hash.length / 8; i++) {
-    var base = (i-1)*8;
-    pathNodes[i]='Q '+parseInt(hash.slice(base,base+2),16)
-      +','+parseInt(hash.slice(base+2,base+4),16)
-      +' '+parseInt(hash.slice(base+4,base+6),16)
-      +','+parseInt(hash.slice(base+6,base+8),16);
+  pathNodes[0] =
+    'M' + parseInt(hash.slice(-4, -2), 16) +
+    ',' + parseInt(hash.slice(-2    ), 16);
+  for(var i = 1; i < qwords; i++) {
+    var base = (i-1) * 8;
+    pathNodes[i] =
+      'Q' + parseInt(hash.slice(base  ,base+2), 16) +
+      ',' + parseInt(hash.slice(base+2,base+4), 16) +
+      ' ' + parseInt(hash.slice(base+4,base+6), 16) +
+      ',' + parseInt(hash.slice(base+6,base+8), 16) ;
   }
   return pathNodes.join(' ');
 }
+
+hashblot.qp = qp;
+hashblot.sha1qp = function(content) {return qp(sha1(content))};
+
+})();
